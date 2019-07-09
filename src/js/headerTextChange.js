dcc32aac0ea853a2e9f3141df2190f3522bc53a6
PavelLazarchuk <pashalazarchuk@gmail.com>
2019-07-09 11:34:30 +0300

    change navbar logo

---
+++ src/js/headerTextChange.js
@@ -0,0 +1,5 @@
+if($(window).width() < 481) {
+    $("#header__description_change").text("A Real Gamechanger In The World Of Web-Development");
+} else {
+    $("#header__description_change").text("A real gamechanger in the world of web development");
+}
